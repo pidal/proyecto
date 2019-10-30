df0b113dc63e0097ef3a74b6e2955ed6ce0f2bee
Oswaldo C <oswaldo1903@gmail.com>
2019-10-30 13:19:29 -0400

    go

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -30,7 +30,7 @@ class AlumnosController extends Controller
             ->where('users.roles_id', User::ROLE_ALUMNO)
             //->join('rel_users_subject', 'rel_users_subject.users_id', '=', 'users.id')
             //->groupBy('users.id')
-            ->orderBy('users.id');
+            ->orderBy('users.id')->get()->dd();
 
         if (isset($request->subject)) {
             //$alumnos = $alumnos->where('rel_users_subject.subject_id', $request->subject);
