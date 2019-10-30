43f57d641d9844cdcb3378153623dedf36e3fba5
Oswaldo C <oswaldo1903@gmail.com>
2019-10-30 16:14:51 -0400

    go

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -152,11 +152,10 @@ class StudentController extends Controller
             ->where('student_files.users_id', auth()->id());
 
         $studentsFiles = Assignment::select('student_files.*')
-
             ->join('student_files', 'assignment.id', '=', 'student_files.assignment_id')
             ->where('assignment.id', $request->assignment_id)
-            ->where('student_files.users_id', auth()->id())
-            ->union($studentsUnion)
+            //->where('student_files.users_id', auth()->id())
+            //->union($studentsUnion)
             ->get();
 
             dd($studentsFiles);
