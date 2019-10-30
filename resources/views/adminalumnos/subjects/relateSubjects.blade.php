99cf3e8b65e4b8415ad85c99ab13b0eb3f1932b7
Oswaldo C <oswaldo1903@.com>
2019-10-30 01:24:16 -0400

    cambios

--- resources/views/adminalumnos/subjects/relateSubjects.blade.php
+++ resources/views/adminalumnos/subjects/relateSubjects.blade.php
@@ -49,7 +49,7 @@
                                             <tr>
                                                 <td>{{$user->name}}</td>
                                                 <td>{{$user->email}}</td>
-                                                <td></td>
+                                                <td>{{$user->user->role->name}}</td>
                                                 <td>
                                                     <div style="display: flex">
                                                         <form action="{{route('relatedUserdestroy', $user->id)}}"
