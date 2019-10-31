1489d3345202f0ff8a7976ed8bfe02bea83cce82
Oswaldo C <oswaldo1903@gmail.com>
2019-10-31 14:54:21 -0400

    go

--- resources/views/subjects/create.blade.php
+++ resources/views/subjects/create.blade.php
@@ -1,14 +1,20 @@
 @extends ('layouts.templateProfesor')
 
-
-
-
 @section('content')
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-sm-8 col-sm-offset-2" style="margin-top: 140px">
                 <div>
-                   
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Error!</strong> Revise los campos obligatorios.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     @if(Session::has('success'))
                         <div class="alert alert-info">
                             {{Session::get('success')}}
@@ -29,33 +35,18 @@
                                         <input type="text" name="name" id="name"
                                                class="col-sm-6 form-control input-sm" placeholder="{{__('subjects.new_name')}}">
                                     </div>
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('name') }}</strong>
-                                            </span>
-                                    @endif
 
                                     <div class="form-group row">
                                         <label class="col-sm-2 col-form-label" for="name"> {{__('subjects.new_grade')}} </label>
                                         <input type="number" name="grade" id="grade"
                                                class="col-sm-6 form-control input-sm" placeholder="{{__('subjects.new_grade')}}">
                                     </div>
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('grade') }}</strong>
-                                            </span>
-                                    @endif
 
                                     <div class="form-group row">
                                         <label class="col-sm-2 col-form-label" for="name"> {{__('subjects.new_description')}} </label>
                                         <textarea name="description" class="col-sm-6 form-control input-sm"
                                                   placeholder="{{__('subjects.new_description')}}"></textarea>
                                     </div>
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('description') }}</strong>
-                                            </span>
-                                    @endif
                                     <div class="form-group row">
                                         <a href="{{ route('subjects.index') }}"
                                            class="btn btn-info col-sm-2 link"> {{__('subjects.back')}}</a>
