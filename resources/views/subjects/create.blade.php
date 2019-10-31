0a9bfc47d0a5400b36408518060d55e883bc0b2e
Oswaldo C <oswaldo1903@gmail.com>
2019-10-31 14:50:43 -0400

    go

--- resources/views/subjects/create.blade.php
+++ resources/views/subjects/create.blade.php
@@ -5,22 +5,6 @@
         <div class="row justify-content-center">
             <div class="col-sm-8 col-sm-offset-2" style="margin-top: 140px">
                 <div>
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <strong>Error!</strong> Revise los campos obligatorios.<br><br>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    @if(Session::has('success'))
-                        <div class="alert alert-info">
-                            {{Session::get('success')}}
-                        </div>
-                    @endif
-
                     <div class="panel panel-default" >
                         <div class="panel-heading">
                             <h3 class="panel-title">{{__('subjects.new')}}</h3>
