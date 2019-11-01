bbb584729421b6fa6d3180ccd0b1cc5bd3c9b49d
Oswaldo C <oswaldo1903@gmail.com>
2019-11-01 14:12:59 -0400

    go

--- app/Models/PruebasUnitarias.php
+++ app/Models/PruebasUnitarias.php
@@ -6,10 +6,11 @@ class PruebasUnitarias
     {
         $path_completo = storage_path('TODO'.DIRECTORY_SEPARATOR . $studentFile->id . '_' . $studentFile->left_attempts);
         chdir($path_completo);
+        
         $exec = '/usr/bin/gcc ' . $path_completo.DIRECTORY_SEPARATOR.$fileInstructor . ' -I/lib/include -lcunit  -o ' . $path_completo.'/'.$studentFile->fileName;
 
-        
-        dd("ver si cambiaron los permisos");
+        chmod($path_completo.'/'.$studentFile->fileName, 0777);
+        dd("ver si cambiaron los");
         //hasta aqui se sube y guarda el archivo en la carpeta correspondiente (codigo fuente)
 
         //dd($exec); esto compila el del profesor y lo almacena en el estudiante (por que?)
