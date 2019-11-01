9b4756abf2c02df216a250eb69ae2fb6149c6366
Oswaldo C <oswaldo1903@gmail.com>
2019-11-01 08:32:26 -0400

    go

--- app/Models/PruebasUnitarias.php
+++ app/Models/PruebasUnitarias.php
@@ -41,7 +41,7 @@ compilation terminated.
 
         shell_exec($exec);
         //Ejecutamos el archivo creado de la compilación
-        $ejecutable = '.'.$path_completo.'/' . $studentFile->fileName;
+        $ejecutable = $path_completo.'/' . $studentFile->fileName;
         dd($ejecutable);
         $salida = $this->PsExecute($ejecutable);
 
