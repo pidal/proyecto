db3dd5ad80e7f84a24669d76c2c7d45f2be4252a
Oswaldo C <oswaldo1903@.com>
2019-11-02 03:10:29 -0400

    cambios

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -43,7 +43,7 @@ class AlumnosController extends Controller
 
         $alumnos = User::where('users.roles_id', User::ROLE_ALUMNO)->
             join('rel_users_subject', 'rel_users_subject.users_id', '=', 'users.id')->
-            whereIn('subject_id', $my_subjects->implode(', '))->get()->dd();
+            whereIn('subject_id', $my_subjects)->get()->dd();
 
 
         if (isset($request->subject)) {
