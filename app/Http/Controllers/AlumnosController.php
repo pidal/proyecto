49bfcb0856bcce29e12a599973009620afa2d02c
Oswaldo C <oswaldo1903@.com>
2019-11-02 03:01:55 -0400

    cambios

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -38,7 +38,7 @@ class AlumnosController extends Controller
 
         $my_subjects = Subject::whereHas('users', function($q){
             $q->where('users.id', Auth::user()->id);
-        })->pluck('subjects.id');
+        })->pluck('id')->dd();
         $my_subjects = ($my_subjects->isEmpty()) ? collect([0]) : $my_subjects;
 
         $alumnos = User::where('users.roles_id', User::ROLE_ALUMNO)->
