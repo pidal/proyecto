acd92cb6f586a431441d528fb9214bda7450ddb0
Oswaldo C <oswaldo1903@.com>
2019-11-03 17:02:42 -0400

    cambios

--- resources/views/adminalumnos/index.blade.php
+++ resources/views/adminalumnos/index.blade.php
@@ -82,14 +82,48 @@
                                                    href="{{action('AdminAlumnosController@edit', $alumno->id)}}">
                                                     <span class="fa fa-pencil"></span></a>
 
-                                                <form action="{{action('AdminAlumnosController@destroy', $alumno->id)}}"
+                                                <button type="button" class="btn btn-danger btn-xs m-1" data-toggle="modal" data-target="#exampleModalCenter_{{$alumno->id}}">
+                                                    <span class="fa fa-trash"></span>
+                                                </button>
+
+                                                <!-- Modal -->
+                                                <div class="modal fade" id="exampleModalCenter_{{$alumno->id}}" tabindex="-1" role="dialog"
+                                                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                                        <div class="modal-content">
+                                                            <form method="post" action="{{action('AdminAlumnosController@destroy', $alumno->id)}}">
+                                                                @csrf
+                                                                <div class="modal-header bg-danger" style="color: white">
+                                                                    <h5 class="modal-title" id="exampleModalLongTitle">{{__('Advertencia')}}</h5>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <p>Se está procediendo a la eliminación del usuario <b>{{$alumno->name}}</b></p>
+                                                                    <p>si continuas con la eliminación se borrarán todos los registros de la misma (prácticas, asignaturas asociadas...).</p>
+                                                                    <p>¿Estás seguro de continuar?</p>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <input name="_method" type="hidden" value="DELETE">
+                                                                    <input type="hidden" name="user_id" value="{{$alumno->id}}"/>
+                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                                                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+                                                <!--<form action="{{action('AdminAlumnosController@destroy', $alumno->id)}}"
                                                       method="post">
                                                     {{csrf_field()}}
                                                     <input name="_method" type="hidden" value="DELETE">
                                                     <button class="btn btn-danger btn-xs m-1" type="submit">
                                                         <span class="fa fa-trash"></span>
                                                     </button>
-                                                </form>
+                                                </form>-->
                                             </div>
 
                                         </td>
