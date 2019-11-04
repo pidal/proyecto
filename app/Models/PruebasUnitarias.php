aae2641f68bf856d7833757535adca74dbb26973
Oswaldo C <oswaldo1903@gmail.com>
2019-11-04 13:43:36 -0400

    go

--- app/Models/PruebasUnitarias.php
+++ app/Models/PruebasUnitarias.php
@@ -7,7 +7,7 @@ class PruebasUnitarias
         $path_completo = storage_path('TODO'.DIRECTORY_SEPARATOR . $studentFile->id . '_' . $studentFile->left_attempts);
         chdir($path_completo);
 
-        $exec = 'sudo /usr/bin/gcc ' . $path_completo.DIRECTORY_SEPARATOR.$fileInstructor . ' -I/lib/include -lcunit  -o ' . $path_completo.'/'.$studentFile->fileName;
+        $exec = 'sudo /usr/bin/gcc ' . $path_completo.DIRECTORY_SEPARATOR.$fileInstructor . ' -I/lib/include -lcunit -o ' . $path_completo.'/'.$studentFile->fileName;
 
         chmod($path_completo.'/'.$fileInstructor, 0777);
         chmod($path_completo.'/'.$studentFile->fileName, 0777);
