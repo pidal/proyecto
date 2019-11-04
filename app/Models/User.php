1a492023e8050dc3051a1bfc8dfb54ef55e293b0
Oswaldo C <oswaldo1903@gmail.com>
2019-11-04 13:12:09 -0400

    go

--- app/Models/User.php
+++ app/Models/User.php
@@ -111,9 +111,9 @@ Fames congue nascetur erat montes a purus facilisi taciti, donec maecenas ultric
 	public function delete(){
 
 	    //Solo para alumnos
-        if($this->roles_id == ROLE_ALUMNO){
+        if($this->roles_id == 3){
         	dd("aad");
-        	
+
             RelUsersGroup::where('users_id',$this->id)->delete();
             RelUsersSubject::where('users_id',$this->id)->delete();
             StudentFile::where('users_id',$this->id)->delete();
