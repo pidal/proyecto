4e5ff925ac897a3a9fe6f2c9a7272d97b3ac99c7
Oswaldo C <oswaldo1903@gmail.com>
2019-11-04 10:31:29 -0400

    go

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,6 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
     }
 
     /**
@@ -23,6 +22,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (Session::has('errors') && Session::has('redirect')) {
+            //dd(Session::all());
+            return redirect()->to(Session::get('redirect'))->with('errors', Session::get('errors'))
+            ->withInput(Session::getOldInput());
+        }
+        return redirect()->to(url('/login'));
     }
 }
