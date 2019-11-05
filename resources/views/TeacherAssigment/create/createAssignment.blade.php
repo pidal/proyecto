acb631602109ae0349bd747fda50d86e870acafe
Oswaldo C <oswaldo1903@.com>
2019-11-05 02:58:56 -0400

    cambios

--- resources/views/TeacherAssigment/create/createAssignment.blade.php
+++ resources/views/TeacherAssigment/create/createAssignment.blade.php
@@ -264,15 +264,15 @@
                 <div class="stepwizard">
                     <div class="stepwizard-row setup-panel">
                         <div class="stepwizard-step">
-                            <a href="#step-1" type="button" class="btn btn-primary btn-circle" onclick="return false;">1</a>
+                            <a href="#step-1" type="button" class="btn btn-primary btn-circle" onclick="event.preventDefault()">1</a>
                             <p>Paso 1</p>
                         </div>
                         <div class="stepwizard-step">
-                            <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled" onclick="return false;">2</a>
+                            <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled" onclick="event.preventDefault()">2</a>
                             <p>Paso 2</p>
                         </div>
                         <div class="stepwizard-step">
-                            <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled" onclick="return false;">3</a>
+                            <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled" onclick="event.preventDefault()">3</a>
                             <p>Paso 3</p>
                         </div>
                     </div>
