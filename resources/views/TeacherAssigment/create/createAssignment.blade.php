b38867b9ca4110fffdcca1e40e140a9fed29746b
Oswaldo C <oswaldo1903@.com>
2019-11-06 02:19:11 -0400

    cambios

--- resources/views/TeacherAssigment/create/createAssignment.blade.php
+++ resources/views/TeacherAssigment/create/createAssignment.blade.php
@@ -3,9 +3,8 @@
 @section('styles')
 
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.12.4.js"></script>
   <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
- 
 
 <style>
 
@@ -323,6 +322,7 @@
 @endsection
 
 @section('scripts')
+
 <script>
 $( "#delivered_date" ).datepicker();
 </script>
