63181014a03e1c50255c08ca0a7d652138f1e6be
Oswaldo C <oswaldo1903@gmail.com>
2019-11-09 09:52:08 -0400

    go

--- resources/views/TeacherAssigment/create/createAssignment-Step3.blade.php
+++ resources/views/TeacherAssigment/create/createAssignment-Step3.blade.php
@@ -4,7 +4,7 @@
 
         <div class="form-group" style="margin-top: 20px">
             <label for="delivered_date">{{ __('Fecha de entrega') }}</label>
-            <input id="delivered_date" type="text" min="<?php echo date();?>"
+            <input id="delivered_date" type="text" min="{{ Carbon::now() }}"
                    class="form-control{{ $errors->has('delivered_date') ? ' is-invalid' : '' }}"
                    name="delivered_date" value="{{ old('delivered_date') }}" required>
             @if ($errors->has('delivered_date'))
