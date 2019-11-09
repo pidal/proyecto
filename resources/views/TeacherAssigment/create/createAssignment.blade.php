d7f7db0daf483064731a0b8250c4ed05c2a68009
Oswaldo C <oswaldo1903@gmail.com>
2019-11-09 10:02:34 -0400

    go

--- resources/views/TeacherAssigment/create/createAssignment.blade.php
+++ resources/views/TeacherAssigment/create/createAssignment.blade.php
@@ -333,7 +333,7 @@ jSuites.calendar(document.getElementById('delivered_date'), {
     format:'DD/MM/YYYY HH24:MI',
     today:0,
     onclose:function() {
-        console.log( this.getValue() );
+        console.log( this );
     }
 });
 </script>
