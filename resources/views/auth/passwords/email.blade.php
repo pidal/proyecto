caa0a8a8456276d4a44d92d2e1a06c8b4fd44b47
Oswaldo C <oswaldo1903@gmail.com>
2019-11-16 10:16:05 -0400

    go

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,23 +9,15 @@
                 <form method="POST" action="{{ route('newpassword') }}">
                     @csrf
 
-                    
-
-                    <div class="form-group row">
-                        <label for="email" class="col-md-2 col-form-label text-md-right">{{ __('auth.email') }}</label>
-
-                        <div class="col-md-10">
-                            <input id="email" type="email"
-                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-                                   value="{{ old('email') }}" required>
-
-                            @if ($errors->has('email'))
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                    <div class="form-group">
+                        <label for="email">{{ __('auth.email') }}</label>
+                        <input type="email" class="form-control" id="email" placeholder="Email" name="email" value="{{ old('email') }}" required>
                     </div>
+                    @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                    @endif
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-2">
                             <button type="submit" class="btn btn-primary">
