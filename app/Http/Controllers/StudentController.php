49d7f7f96d2405a2fd6dba90d8fbf9ed6720bdb7
Oswaldo C <oswaldo1903@gmail.com>
2019-11-16 11:29:29 -0400

    go

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -132,9 +132,7 @@ class StudentController extends Controller
 			->where('assignment.subject_id', $request->subject_id)
 			->distinct('assignment.id')
             ->union($assignments)
-			->paginate(1);
-
-			dd($assignments);
+			->dd();
 
 		$subject = Subject::where('id', $request->subject_id)
             ->select('name','id')
