6cc413f6ec3741d3ac267bbf1e0019b58fc94ab7
Oswaldo C <oswaldo1903@gmail.com>
2019-11-21 14:07:42 -0400

    go

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     public function index()
     {
         if (Session::has('errors') && Session::has('redirect')) {
-            //dd(Session::all());
+            dd(Session::all());
             return redirect()->to(Session::get('redirect'))->with('errors', Session::get('errors'))
             ->withInput(Session::getOldInput());
         }
