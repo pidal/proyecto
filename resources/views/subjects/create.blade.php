6d2484615174f44f383464ff924fb802d9daa75c
Oswaldo C <oswaldo1903@gmail.com>
2019-11-22 15:57:05 -0400

    go

--- resources/views/subjects/create.blade.php
+++ resources/views/subjects/create.blade.php
@@ -85,6 +85,46 @@
     <div class="row justify-content-center">
         <div class="col-sm-4 col-sm-offset-4" style="margin-top: 100px">
             <h2>Crear asignatura</h2>
+
+             @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        Has introducido mal el Email y/o la contraseña .<br><br>
+                    </div>
+                @endif
+                @if ( Session::has('success') )
+                    <div class="alert alert-success alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            <span class="sr-only">Close</span>
+                        </button>
+                        <strong>{{ Session::get('success') }}</strong>
+                    </div>
+                @endif
+                @if ( Session::has('error') )
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            <span class="sr-only">Close</span>
+                        </button>
+                        <strong>{{ Session::get('error') }}</strong>
+                    </div>
+                @endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
             <form class="form-horizontal" method="POST" action="{{ route('subjects.store') }}" role="form">
                 @csrf
 
