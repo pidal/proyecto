a6fe7d93ba6c2531ce412cf12848a7f80d720dab
Oswaldo C <oswaldo1903@.com>
2019-11-27 00:30:34 -0400

    cambios

--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -119,7 +119,10 @@ class SubjectsController extends Controller
             'grade' => 'required',
             'imagen' => 'nullable|image|mimes:jpeg,jpg,png,gif|max:1024'
         ]);
-        $subject = Subject::find($id)->update($request->all());
+
+        $subject = Subject::find($id);
+        $subject->update($request->all());
+
         if($request->hasFile('imagen')){
             $imagen = $request->file('imagen');
             $imagenName = $subject->id . '_' . time() . '.' . $imagen->getClientOriginalExtension();
