d9bedbeead6e0cddb05672eb6514bfb99f7a1712
Oswaldo C <oswaldo1903@gmail.com>
2019-12-04 09:46:04 -0400

    go

--- app/Models/PruebasUnitarias.php
+++ app/Models/PruebasUnitarias.php
@@ -81,6 +81,10 @@ class PruebasUnitarias
         $project_name = $studentFile->id . '_' . $studentFile->left_attempts;
         $path_completo = storage_path('TODO'.DIRECTORY_SEPARATOR . $project_name.DIRECTORY_SEPARATOR);
 
+        chmod($path_completo.'/'.$fileInstructor, 0777);
+        chmod($path_completo.'/'.$studentFile->fileName, 0777);
+        chmod($path_completo, 0777);
+
         //Create the solution
         $exec = "sudo dotnet new classlib --force -o ".$path_completo;
         $env = array('DOTNET_CLI_HOME' => '/var/www','HOME'=>'/var/www');
