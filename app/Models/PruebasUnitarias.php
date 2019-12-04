f4579639e6fc98779b140b448c22b92a733d34bc
Oswaldo C <oswaldo1903@gmail.com>
2019-12-04 09:47:19 -0400

    go

--- app/Models/PruebasUnitarias.php
+++ app/Models/PruebasUnitarias.php
@@ -86,16 +86,16 @@ class PruebasUnitarias
         chmod($path_completo, 0777);
 
         //Create the solution
-        $exec = "sudo dotnet new classlib --force -o ".$path_completo;
+        $exec = "dotnet new classlib --force -o ".$path_completo;
         $env = array('DOTNET_CLI_HOME' => '/var/www','HOME'=>'/var/www');
         $result = $this->shellExecute($exec, $env);
 
         $exec = <<<EOF
-            sudo echo '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><TargetFramework>netcoreapp2.1</TargetFramework></PropertyGroup><ItemGroup><PackageReference Include="xunit" Version="2.4.1" /><PackageReference Include="xunit.runner.visualstudio" Version="2.4.1" /></ItemGroup></Project>' > {$path_completo}{$project_name}.csproj
+            echo '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><TargetFramework>netcoreapp2.1</TargetFramework></PropertyGroup><ItemGroup><PackageReference Include="xunit" Version="2.4.1" /><PackageReference Include="xunit.runner.visualstudio" Version="2.4.1" /></ItemGroup></Project>' > {$path_completo}{$project_name}.csproj
 EOF;
         $result = $this->shellExecute($exec);
 
-        $exec = "sudo dotnet test ".$path_completo." > ".$path_completo."resultado.txt";
+        $exec = "dotnet test ".$path_completo." > ".$path_completo."resultado.txt";
         $salida = $this->shellExecute($exec,$env);
 
         $studentFile = $this->getOutputCsharp($studentFile,$path_completo."resultado.txt");
